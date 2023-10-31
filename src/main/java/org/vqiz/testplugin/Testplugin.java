25fbda72fa90822703c3f01ad1a61cb128c055b3
vqiz <102254522+vqiz@users.noreply.github.com>
2023-10-31 21:34:11 +0100

    Initial commit

---
+++ src/main/java/org/vqiz/testplugin/Testplugin.java
@@ -0,0 +1,17 @@
+package org.vqiz.testplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Testplugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        System.out.println("enabled 111111111111111");
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}

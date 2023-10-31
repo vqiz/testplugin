17825bdcf67cc0c9357cb641114813f458521a16
vqiz <102254522+vqiz@users.noreply.github.com>
2023-10-31 23:03:31 +0100

    uzgfadasdasd

--- src/main/java/org/vqiz/testplugin/Testplugin.java
+++ src/main/java/org/vqiz/testplugin/Testplugin.java
@@ -6,7 +6,7 @@ public final class Testplugin extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        System.out.println("enabled 111111111111111");
+        System.out.println("enabled 111111111111");
         System.out.println("adolf");
         System.out.println("tst");
     }

1bb7edeb91ef0175cf384a86717ad196c7223705
vqiz <102254522+vqiz@users.noreply.github.com>
2023-11-01 18:54:53 +0100

    uzgfadasdasdasd

--- src/main/java/org/vqiz/testplugin/Testplugin.java
+++ src/main/java/org/vqiz/testplugin/Testplugin.java
@@ -7,7 +7,7 @@ public final class Testplugin extends JavaPlugin {
     @Override
     public void onEnable() {
         System.out.println("enabled 111111111111");
-        System.out.println("a           asdasddolf");
+        System.out.println("a            asdasddolf");
         System.out.println("tst");
     }
 

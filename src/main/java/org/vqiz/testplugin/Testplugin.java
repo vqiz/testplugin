6072bb1d468f3f44564cb6d1f2c73e1fe2a94ff6
vqiz <102254522+vqiz@users.noreply.github.com>
2023-11-10 19:21:43 +0100

    uzgfadasdasdasdgfdefwscdvxfgdf

--- src/main/java/org/vqiz/testplugin/Testplugin.java
+++ src/main/java/org/vqiz/testplugin/Testplugin.java
@@ -1,8 +1,16 @@
 package org.vqiz.testplugin;
 
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.vqiz.space.Space;
 
-public final class Testplugin extends JavaPlugin {
+import java.util.List;
+import java.util.UUID;
+
+public final class Testplugin extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
